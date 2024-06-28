78b146ab16d91e2a88f1e3e3582ddf8172e9f092
suman <sumanj631@gmail.com>
2024-06-28 22:50:06 +0530

    .

--- src/socket/SocketProvider.js
+++ src/socket/SocketProvider.js
@@ -3,7 +3,7 @@ import { Spinner } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import io from 'socket.io-client';
 
-const url = process.env.REACT_APP_LOCAL_SOCKET_URL;
+const url = process.env.REACT_APP_SOCKET_URL;
 
 const SocketContext = createContext();
 
